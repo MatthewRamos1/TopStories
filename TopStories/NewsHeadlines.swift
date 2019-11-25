3d70bbad8b566d42c0b4d4bb43a425a1775ff9a4
Matthew Ramos <mmramos309@gmail.com>
2019-11-25 12:26:27 -0500

    Created json file and copied over data, created headlines object

---
+++ TopStories/NewsHeadlines.swift
@@ -0,0 +1,13 @@
+//
+//  NewsHeadlines.swift
+//  TopStories
+//
+//  Created by Matthew Ramos on 11/25/19.
+//  Copyright © 2019 Matthew Ramos. All rights reserved.
+//
+
+import Foundation
+
+struct HeadlinesData {
+    
+}
